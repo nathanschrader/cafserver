ff2a35d03090a267a64d73e8b151e3e40315cbb5
Nathan Schrader <nathan.schrader@eagles.oc.edu>
2019-01-16 16:37:29 -0600

    basic functionality

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log("This is the first commit!");
+//-------------------------------------------------------------------------
+// Server component of the Mobile App Development group project
+//
+//-------------------------------------------------------------------------
+
+
+/*
+  This tutorial referenced:
+  https://www.tutorialspoint.com/nodejs/nodejs_restful_api.htm
+*/
+
+var vote = 0;
+var express = require('express');
+var app = express();
+
+app.get('/getCount', function (req, res) {
+
+      res.end(JSON.stringify(vote));
+})
+
+app.post('/upvote', function (req, res) {
+   // First read existing users.
+      vote = vote + 1;
+      console.log("upvoted: " + vote);
+      res.end();
+})
+
+app.post('/downvote', function (req, res) {
+   // First read existing users.
+      vote = vote - 1;
+      console.log("downvoted: " + vote);
+      res.end();
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})

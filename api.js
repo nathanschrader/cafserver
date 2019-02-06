e3b06951b1148100a976a88c6f140c07bd1cf178
nathan schrader <nathan.schrader@eagles.oc.edu>
2019-02-06 13:36:25 -0600

    fixed spelling

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-function cafateria(){
+function cafeteria(){
   this.votes = [],
   this.getScore = function(){
     var score = 0;
@@ -16,13 +16,58 @@ function cafateria(){
   }
 };
 
-var mainLine = new cafateria();
+var mainLine = new cafeteria();
+var chefLine = new cafeteria();
+var glutenFree = new cafeteria();
+var sandwichLine = new cafeteria();
+var wrapLine = new cafeteria();
+
 
 app.get('/getMainlineScore', function (req, res) {
   res.end(mainLine.getScore().toString());
 });
 
+app.get('/getCheflineScore', function (req, res) {
+  res.end(chefLine.getScore().toString());
+});
+
+app.get('/getGlutenFreeScore', function (req, res) {
+  res.end(glutenFree.getScore().toString());
+});
+
+app.get('/getSandwichlineScore', function (req, res) {
+  res.end(sandwichLine.getScore().toString());
+});
+
+app.get('/geWraplineScore', function (req, res) {
+  res.end(wrapLine.getScore().toString());
+});
+
 app.post("/setMainLineScore/:vote", function(req, res){
+  mainLine.vote(parseInt(req.params.vote));
+  res.sendStatus(200);
+  console.log(req.params.vote);
+});
+
+app.post("/setChefLineScore/:vote", function(req, res){
+  mainLine.vote(parseInt(req.params.vote));
+  res.sendStatus(200);
+  console.log(req.params.vote);
+});
+
+app.post("/setGlutenFreeScore/:vote", function(req, res){
+  mainLine.vote(parseInt(req.params.vote));
+  res.sendStatus(200);
+  console.log(req.params.vote);
+});
+
+app.post("/setSandwichLineScore/:vote", function(req, res){
+  mainLine.vote(parseInt(req.params.vote));
+  res.sendStatus(200);
+  console.log(req.params.vote);
+});
+
+app.post("/setWrapLineScore/:vote", function(req, res){
   mainLine.vote(parseInt(req.params.vote));
   res.sendStatus(200);
   console.log(req.params.vote);
